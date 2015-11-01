7313d27148b51dbc2844b3127ea4ee9c73fc67a6
LeVar Gilford <lgilford@gmail.com>
2015-11-01 12:42:23 -0500

    Homework 1

---
+++ hw1-1.rb
@@ -0,0 +1,9 @@
+puts "What is the first number?"
+fnum = gets.chomp.to_i
+
+puts "What is the second number?"
+snum = gets.chomp.to_i
+
+sum = fnum + snum
+
+puts "The sum of the numbers #{fnum} and #{snum} is #{sum}."
